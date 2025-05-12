878390eae59415403c2a75ccdebb968e42258196
a7mdragab1 <a7mdragab1@gmail.com>
2025-05-13 01:49:19 +0300

    Add new files

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "947839564"
+    serviceWorkerVersion: "2168633702"
   }
 });
